35e4974e1ef990740803f1b690a368486d475ae0
Daniel Antony Vallejos Barboza <mvcs_ogmei_davb@vivienda.gob.pe>
2024-05-28 14:34:46 -0500

    avance funcional

--- src/js/mat.js
+++ src/js/mat.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([-9.19, -75.015], 6);
+var map = L.map('map').setView([-10.450622241808368, -74.63887799107934], 5.5);
 
 L.tileLayer('', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -6,13 +6,15 @@ L.tileLayer('', {
 
 // Estilo para los departamentos
 function style(feature) {
+    var departamento = feature.properties.NOMBDEP.trim().toUpperCase();
+    var fillColor = departamentosPeru.includes(departamento) ? '#198754' : '#C00000';
+    
     return {
-        fillColor: '#C00000',
+        fillColor: fillColor,
         weight: 2,
-        opacity: 1,
+        opacity: 4,
         color: 'white',
-        dashArray: '3',
-        fillOpacity: 0.7
+        fillOpacity: 1
     };
 }
 
@@ -20,13 +22,13 @@ function onEachFeature(feature, layer) {
     var centroid = layer.getBounds().getCenter();
 
     // Crear una etiqueta con el nombre del departamento
-    /* var label = L.marker(centroid, {
+    var label = L.marker(centroid, {
         icon: L.divIcon({
             className: 'label',
             html: feature.properties.NOMBDEP,
-            //iconSize: [70, 30]
+            iconSize: [70, 30],
         })
-    }).addTo(map); */
+    }).addTo(map);
 
     layer.on('click', function () {
         var departamento = feature.properties.NOMBDEP.trim().toUpperCase();
@@ -97,3 +99,4 @@ $(document).on("click", ".clickDepartamento", function () {
         $("#body").load(linkPath[indice]);
     }, 300);
 });
+
