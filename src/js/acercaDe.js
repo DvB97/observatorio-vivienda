e50d7b0fd288f56b7cf27c284c564a4d0e854546
Daniel Antony Vallejos Barboza <mvcs_ogmei_davb@vivienda.gob.pe>
2024-06-11 15:26:53 -0500

    apartado de ACERCA DE v1 terminado

---
+++ src/js/acercaDe.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    console.log('hola munod');
+    $("#lineaDeTiempo").load("./src/views/comp/acerca_de/lineaDeTiempo.html");
+
+});
--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,7 @@
 $(document).ready(function () {
     $("#menuContainer").load("./src/views/menu.html");
     $("#body").load("./src/views/body.html"); 
-    //$("#body").load("./src/views/comp/oei/uno/uno.html");
+    //$("#body").load("./src/views/comp/acerca_de/acercaDe.html");
     $("#footerContainer").load("./src/views/footer.html");
 });
 
